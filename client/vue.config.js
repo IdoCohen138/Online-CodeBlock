517199b77ea61da2379b0999513ae3ae05676ea8
idoco <idocohen138@gmail.com>
2023-12-01 15:48:26 +0200

    port change

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     devServer: {
-        host: 'client-production-a5fb.up.railway.app', 
+        // host: 'client-production-a5fb.up.railway.app', 
         port: '8080', 
     },
 };
