b6237c52a2fa45fe8035ba7e3eeeb2be4a7e0d2c
idoco <idocohen138@gmail.com>
2023-12-01 14:41:07 +0200

    add port env para

--- server/db/db.js
+++ server/db/db.js
@@ -4,12 +4,11 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     connectionLimit: 10,
   });
-console.log(process.env);
+
 const promisePool = pool.promise();
 
-  
-  
 module.exports={promisePool}
